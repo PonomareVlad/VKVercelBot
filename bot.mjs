c993db02956ad876e1c18a2c8bef1353d29e5f4b
PonomareVlad <Git@PonomareVlad.ru>
2022-04-09 16:33:44 +0500

    New message event used

--- bot.mjs
+++ bot.mjs
@@ -3,4 +3,4 @@ import {API, Upload, Updates} from 'vk-io';
 const api = new API({token: process.env.TOKEN});
 
 export default new Updates({api, upload: new Upload({api})})
-    .on('message', async context => await context.send(context.text));
+    .on('message_new', async context => await context.send(context.text));
