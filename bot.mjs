5b291a28be81fc2cb29695fbf5f912a744e48dda
PonomareVlad <Git@PonomareVlad.ru>
2022-04-09 16:15:46 +0500

    Echo bot demo

--- bot.mjs
+++ bot.mjs
@@ -2,12 +2,5 @@ import {API, Upload, Updates} from 'vk-io';
 
 const api = new API({token: process.env.TOKEN});
 
-const upload = new Upload({api});
-
-const updates = new Updates({api, upload, webhookConfirmation: process.env.CONFIRMATION});
-
-updates.on('message', (context) => {
-    console.debug(context)
-});
-
-export default updates;
+export default new Updates({api, upload: new Upload({api})})
+    .on('message', async context => await context.send(context.text));
